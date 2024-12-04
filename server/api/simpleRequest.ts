fb0644752d59d74312cf1007885f3716c8f9c8b9
Dima Lishnovskiy <lishnovskiy.dima@gmail.com>
2024-12-04 14:24:59 +0200

    fe-1-test/commit-1-added-log-from-fe-1-test-branch

--- server/api/simpleRequest.ts
+++ server/api/simpleRequest.ts
@@ -1,6 +1,25 @@
 export default defineEventHandler(async (event) => {
   console.log('request from server');
 
+//   const db = hubDatabase()
+
+//   console.log('db:', db);
+
+//   const result = await db.prepare('SELECT * FROM DB').all();
+//   const result = await db.prepare('SELECT * FROM nuxt-hub-test-1').all();
+//   const result = await db.prepare('SELECT * FROM nuxtHubTest1').all();
+//   const result = await db.prepare('SELECT * FROM Customers').all();
+// const result = await db.prepare('SELECT * FROM _hub_migrations').all();
+
+// const { results } =  db.prepare('SELECT * FROM _hub_migrations').all();
+
+  
+ 
+//   console.log('result:', result);
+
+//     console.log('results:', results);
+  
+
    return { message: 'Hello from Simple request!' }
   
 })
